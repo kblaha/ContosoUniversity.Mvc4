1993e71b54c9ef6849869e4c1482bc9fdce162ce
kblaha <kblaha@gmail.com>
2014-02-09 09:44:15 -0800

    Remove convention to pluralize table names when generating database.

--- ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
-using ContosoUniversity.Models;
-
-namespace ContosoUniversity.DAL
+﻿namespace ContosoUniversity.DAL
 {
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+    using ContosoUniversity.Models;
+
     public class SchoolContext : DbContext
     {
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
