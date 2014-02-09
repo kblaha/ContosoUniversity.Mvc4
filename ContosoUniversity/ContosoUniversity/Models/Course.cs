63504f130693db57c4ae4bfd8fcd7e68983efbd4
kblaha <kblaha@gmail.com>
2014-02-09 09:38:29 -0800

    Add entity model classes.

---
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace ContosoUniversity.Models
+{
+    using System.Collections.Generic;
+
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace ContosoUniversity.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
