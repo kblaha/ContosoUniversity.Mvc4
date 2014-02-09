636d05d92e361ae80a27a1999c2ce45fe2d654cc
kblaha <kblaha@gmail.com>
2014-02-09 09:39:44 -0800

    Allow course IDs to be preset by user rather than auto-generated by database.

--- ContosoUniversity/ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/ContosoUniversity/Models/Course.cs
@@ -1,9 +1,11 @@
 ﻿namespace ContosoUniversity.Models
 {
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Course
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseId { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
